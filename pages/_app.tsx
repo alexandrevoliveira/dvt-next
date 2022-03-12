9b92395719fd6340c20bea0edf1189370cbf95e5
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2022-03-11 21:56:10 -0300

    feat: initial configs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
-import '../styles/globals.css'
+import { theme } from '../styles/theme'
 import type { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
+
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider resetCSS theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
