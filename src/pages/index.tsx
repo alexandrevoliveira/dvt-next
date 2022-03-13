9afeca755401e3148e7fe9e80f09f0130fead61c
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2022-03-13 19:58:28 -0300

    feat: header component inserted into main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,10 @@
 import { Box, Flex, Heading, Text } from "@chakra-ui/react";
-import Head from "next/head";
+import { Header } from "../components/Header";
 
 export default function Home() {
   return (
     <>
-      <Head>
-          <title>VET</title>
-      </Head>
+      <Header />
 
       <Flex>
         <Box>
