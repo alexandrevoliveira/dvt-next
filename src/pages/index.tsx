969046c1807274023edc7b16fbf94029beea6ac9
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2022-03-13 22:11:05 -0300

    feat: addition of the Input component into the Home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,19 @@
-import { Box, Flex, Heading, Text } from "@chakra-ui/react";
+import { Stack } from "@chakra-ui/react";
 import { PageCompose } from "../components/Compose/PageCompose";
-import { Header } from "../components/Header";
+import { Input } from "../components/Form/Input";
 
 export default function Home() {
   return (
     <PageCompose header_title="VET | Home">
-      <Box>data</Box>
+      <Stack>
+        <Input
+          name="dvtfile"
+          label="Arquivo"
+          type="file"
+          size="sm"
+          accept=".csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel"
+        />
+      </Stack>
     </PageCompose>
   )
 }
