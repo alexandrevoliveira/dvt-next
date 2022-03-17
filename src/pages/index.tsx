0c9b79a13e17bf3d550ab22dcb483ce2b3180655
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2022-03-16 23:08:21 -0300

    feat: addition of new FileInput component on Home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,30 @@
-import { Stack } from "@chakra-ui/react";
+import { Flex } from "@chakra-ui/react";
 import { PageCompose } from "../components/Compose/PageCompose";
-import { Input } from "../components/Form/Input";
+import { FileInputButton } from "../components/Form/FileInputButton";
+import { uploadFileRequest } from "../domains/upload.services";
 
 export default function Home() {
+  const onChange = async (formData: FormData) => {
+    const response = await uploadFileRequest(formData, (event) => {
+      console.log(`Current progress:`, Math.round((event.loaded * 100) / event.total));
+    });
+
+    console.log('response', response);
+  };
+
   return (
     <PageCompose header_title="VET | Home">
-      <Stack>
-        <Input
-          name="dvtfile"
-          label="Arquivo"
-          type="file"
-          size="sm"
-          accept=".csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel"
+      <Flex
+        mt={4}
+        justify="center"
+        align="center"
+      >
+        <FileInputButton
+          label="Upload Single File"
+          uploadFileName="files"
+          onChange={onChange}
         />
-      </Stack>
+      </Flex>
     </PageCompose>
   )
 }
