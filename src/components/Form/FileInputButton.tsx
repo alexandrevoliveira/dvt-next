f991b74eac664b3649773dfb03fc54e1c6464a9e
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2022-03-16 22:58:38 -0300

    feat: addition of the FileInput component

---
+++ src/components/Form/FileInputButton.tsx
@@ -0,0 +1,71 @@
+import { Button, Icon } from '@chakra-ui/react';
+import { ChangeEvent, useRef } from 'react';
+import { RiFileUploadFill } from "react-icons/ri"
+
+export interface UiFileInputButtonProps {
+  acceptedFileTypes?: string;
+  allowMultipleFiles?: boolean;
+  label: string;
+  onChange: (formData: FormData) => void;
+  uploadFileName: string;
+}
+
+export const FileInputButton = 
+  ({ label, onChange, uploadFileName, acceptedFileTypes, allowMultipleFiles}: UiFileInputButtonProps) => {
+  const fileInputRef = useRef<HTMLInputElement | null>(null);
+  const formRef = useRef<HTMLFormElement | null>(null);
+
+  const onClickHandler = () => {
+    fileInputRef.current?.click();
+  };
+
+  const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    if (!event.target.files?.length) {
+      return;
+    }
+
+    const formData = new FormData();
+
+    Array.from(event.target.files).forEach((file) => {
+      formData.append(event.target.name, file);
+    });
+
+    onChange(formData);
+
+    formRef.current?.reset();
+  };
+
+  return (
+    <form ref={formRef}>
+      <Button
+        type="button"
+        onClick={onClickHandler}
+        color="white"
+        bgColor="purple.dvt-mid"        
+        borderColor="white"
+        borderWidth={1}
+        _hover={{
+          bgColor: "purple.dvt-dark",
+          borderColor: "white.800"
+        }}
+        rightIcon={<Icon as={RiFileUploadFill} fontSize={24}/>}
+      >
+        {label}
+      </Button>
+      <input
+        accept={acceptedFileTypes}
+        multiple={allowMultipleFiles}
+        name={uploadFileName}
+        onChange={onChangeHandler}
+        ref={fileInputRef}
+        style={{ display: 'none' }}
+        type="file"
+      />
+    </form>
+  );
+};
+
+FileInputButton.defaultProps = {
+  acceptedFileTypes: '',
+  allowMultipleFiles: false,
+};
