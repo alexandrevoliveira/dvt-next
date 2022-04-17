4b7ebe1a5f08875ce66d233b329210d3df2a9ceb
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2022-04-17 10:59:38 -0300

    feat: next-auth provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,15 +2,17 @@ import { theme } from '../styles/theme'
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import { SidebarDrawerProvider } from '../contexts/SidebarDrawerContext'
-
+import { SessionProvider as NextAuthProvider } from 'next-auth/react'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider resetCSS theme={theme}>
-      <SidebarDrawerProvider>
-        <Component {...pageProps} />
-      </SidebarDrawerProvider>
-    </ChakraProvider>
+    <NextAuthProvider session={pageProps.session}>
+      <ChakraProvider resetCSS theme={theme}>
+        <SidebarDrawerProvider>
+          <Component {...pageProps} />
+        </SidebarDrawerProvider>
+      </ChakraProvider>
+    </NextAuthProvider>
   )
 }
 
