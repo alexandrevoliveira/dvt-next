a0acce0abdf63693cf4f70d5dfab94e4cf30b9e7
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2022-04-29 15:20:01 -0300

    feat: new home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,5 @@
-import { Stack } from "@chakra-ui/react";
-import { useEffect, useState } from "react";
+import { Heading, Stack } from "@chakra-ui/react";
 import { PageCompose } from "../components/Compose/PageCompose";
-import { FileInput } from "../components/Form/FileInput";
-import { getUploadedFiles, uploadFileRequest } from "../domains/upload.services";
-import { CustomCSVSelect } from "../components/Select/CustomCSVSelect";
-import { shapeCSVLines, shapeEvasionCSVLines } from "../utils/helpers";
-import Dashboard from "../components/Dashboard";
 
 interface FileProps {
   name: string;
@@ -13,64 +7,26 @@ interface FileProps {
 }
 
 export default function Home() {
-  const [filesData, setFilesData] = useState<any[]>()
-  const [file, setFile] = useState<FileProps>()
-  const [fileObject, setFileObject] = useState({})
-
-  const onChange = async (formData: FormData) => {
-    const response = await uploadFileRequest(formData, (event) => {
-      console.log(`Current progress:`, Math.round((event.loaded * 100) / event.total));
-    });
-
-    setFilesData(response.data)
-  };
-
-  useEffect(() => {
-    const response = getUploadedFiles()
-    Promise.resolve(response).then(res => {
-      setFilesData(res.data)
-    }).catch(err => console.error(err))
-  }, [])
-  
-  useEffect(() => {
-    if (file) {
-      const column_names = file.rows[0] as string[]
-      column_names.includes('Período Ingresso')
-      && column_names.includes('Semestre da Evasão')
-      && column_names.includes('Número de evadidos por turma') ?
-        setFileObject(shapeEvasionCSVLines(file.rows))
-        : setFileObject(shapeCSVLines(file.rows))
-
-    } else {
-      return null
-    };
-  }, [file])
   
   return (
-    <PageCompose header_title="VET | Home">
-      <Stack flex="1" direction="column" spacing={[4, 4, 4, 4, 6]} justify="center" align="center">
+    <PageCompose header_title="DVT | Home">
+      <Stack
+        flex="1"
+        direction="column"
+        spacing={[4, 6]}
+        justify="center"
+        align="center"
+      >
         <Stack
           mt={4}
           align="center"
-          direction={["column", "column", "column", "column" ,"row"]}
-          spacing={[4, 4, 4, 4, 16]}
+          direction={"column"}
+          spacing={[10, 12]}
+          color="gray.50"
         >
-          <FileInput
-            label="Upload Single CSV File"
-            uploadFileName="files"
-            onChange={onChange}
-          />
-
-          <CustomCSVSelect
-            value={file}
-            items={filesData}
-            onChange={(newValue) => setFile(newValue)}
-          />
+          <Heading>Hello! Welcome to our data visualization tool.</Heading>
+          <Heading>Please log to be abble to use our graphs through adding any file you want</Heading>
         </Stack>
-        
-        {file && (<Stack w="100%" maxW="680px" h="100%">
-          <Dashboard name={file?.name} object={fileObject}/>
-        </Stack>)}
       </Stack>
     </PageCompose>
   )
